9c4481b09e0d95777daa7fa6f332d2f155cef5da
sindhu-M-Valerie <79721049+sindhu-M-Valerie@users.noreply.github.com>
2025-07-06 18:12:45 +0000

    Fix GitHub Pages blank page issue
    - Changed webpack publicPath from '/' to './' for relative paths
    - Fixed bundle.js loading on GitHub Pages with repository name
    - Updated .nojekyll file placement
    - Site should now load correctly at GitHub Pages URL

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
-    publicPath: '/',
+    publicPath: './',
   },
   module: {
     rules: [
